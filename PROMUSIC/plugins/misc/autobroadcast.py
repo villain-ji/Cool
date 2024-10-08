0e216958d2dbe80144fdd36e2786c40b54c8c42c
Yasir Akhtar <yasiralam662@gmail.com>
2024-10-08 21:33:19 +0530

    Cookies Updated !

--- PROMUSIC/plugins/misc/autobroadcast.py
+++ PROMUSIC/plugins/misc/autobroadcast.py
@@ -1,67 +1,67 @@
 import asyncio
 import datetime
-import logging
 from PROMUSIC import app
 from pyrogram import Client
 from config import START_IMG_URL, IMG_URL
 from PROMUSIC.utils.database import get_served_chats, get_served_users
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.errors import FloodWait, RPCError
+from pyrogram.errors import FloodWait
 
-# Specify the channel ID and message ID you want to forward
-CHANNEL_ID = -1002231749322  # Replace with your channel ID
-MESSAGE_ID = 26  # Replace with the actual message ID from the channel
 
-# Setup logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+MESSAGE = f"""**๏ ᴛʜɪs ɪs ᴛʜᴇ ᴀᴅᴠᴀɴᴄᴇ ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ + ᴍᴀɴᴀɢᴇᴍɴᴇᴛ ʀᴏʙᴏᴛ 💗. 💌
 
-# Specify the channel ID and message ID you want to forward
-CHANNEL_ID = -1002231749322  # Replace with your channel ID
-MESSAGE_ID = 26  # Replace with the actual message ID from the channel
+🎧 ᴘʟᴀʏ + ᴠᴘʟᴀʏ + ᴄᴘʟᴀʏ 🎧
 
-async def forward_message_to_chats():
+➥ sᴜᴘᴘᴏʀᴛᴇᴅ ᴡᴇʟᴄᴏᴍᴇ - ʟᴇғᴛ ɴᴏᴛɪᴄᴇ, ᴛᴀɢᴀʟʟ, ᴠᴄᴛᴀɢ, ʙᴀɴ - ᴍᴜᴛᴇ, sʜᴀʏʀɪ, ʟʏʀɪᴄs, sᴏɴɢ - ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅ, ᴇᴛᴄ... 💕
+
+🔐ᴜꜱᴇ » [/start](https://t.me/{app.username}?start=true) ᴛᴏ ᴄʜᴇᴄᴋ ʙᴏᴛ
+
+➲ ʙᴏᴛ :** @{app.username}"""
+
+BUTTON = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏", url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users")
+        ]
+    ]
+)
+
+CELEBRATION_VID_URL = "https://github.com/doraemon890/ANNIE-X-MUSIC/assets/155803358/dcbec346-bb98-4818-9a1c-683e9c3107b0"
+
+BD_VID = "https://telegra.ph/file/943bb99829ec526c3f99a.mp4"
+
+async def send_message_to_chats():
     try:
         chats = await get_served_chats()
         users = await get_served_users()
 
-        # Forward message to all chats
+        # Send message to chats
         for chat in chats:
             chat_id = int(chat["chat_id"])
             try:
-                await app.forward_messages(chat_id, CHANNEL_ID, MESSAGE_ID)
-                logger.info(f"Forwarded message to chat {chat_id}")
+                await app.send_photo(chat_id, photo=IMG_URL, caption=MESSAGE, reply_markup=BUTTON)
                 await asyncio.sleep(0.2)  # Sleep for 0.2 seconds to avoid FloodWait
             except FloodWait as fw:
-                logger.warning(f"FloodWait: Sleeping for {fw.value} seconds")
                 await asyncio.sleep(fw.value)
-            except Exception as e:
-                logger.error(f"Error forwarding message to chat {chat_id}: {e}")
+            except Exception:
                 continue
 
-        # Forward message to all users
+        # Send message to users
         for user in users:
             user_id = int(user["user_id"])
             try:
-                await app.forward_messages(user_id, CHANNEL_ID, MESSAGE_ID)
-                logger.info(f"Forwarded message to user {user_id}")
+                await app.send_photo(user_id, photo=IMG_URL, caption=MESSAGE, reply_markup=BUTTON)
                 await asyncio.sleep(0.2)  # Sleep for 0.2 seconds to avoid FloodWait
             except FloodWait as fw:
-                logger.warning(f"FloodWait: Sleeping for {fw.value} seconds")
                 await asyncio.sleep(fw.value)
-            except Exception as e:
-                logger.error(f"Error forwarding message to user {user_id}: {e}")
+            except Exception:
                 continue
 
-    except RPCError as e:
-        logger.error(f"RPCError: {e}")
     except Exception as e:
-        logger.error(f"Unexpected Error: {e}")
-
+        pass  # Do nothing if an error occurs while fetching served chats
 async def continuous_broadcast():
     while True:
-        logger.info("Starting broadcast")
-        await forward_message_to_chats()  # Call the forward message function
+        await send_message_to_chats()
         await asyncio.sleep(82800)  # Sleep (82800 seconds) between next broadcast
 
 # Start the continuous broadcast loop
