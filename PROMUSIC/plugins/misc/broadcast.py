d3980c4df3aee078b12f440184cbf0caaddc9245
Yasir Akhtar <yasiralam662@gmail.com>
2024-12-01 19:01:17 +0530

    New

--- PROMUSIC/plugins/misc/broadcast.py
+++ PROMUSIC/plugins/misc/broadcast.py
@@ -23,10 +23,10 @@ IS_BROADCASTING = False
 @app.on_message(filters.command("broadcast") & SUDOERS)
 @language
 async def braodcast_message(client, message, _):
-    # if message.from_user.id != MAIN_OWNER:
-    #     return await message.reply_text(
-    #         f"» 🚀 Wᴀɴᴛ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ʏᴏᴜʀ ᴏᴡɴ ᴍᴇssᴀɢᴇs?\n\nUɴʟᴏᴄᴋ ᴇxᴄʟᴜsɪᴠᴇ ᴀᴄᴄᴇss ᴡɪᴛʜ ᴏᴜʀ sᴜʙsᴄʀɪᴘᴛɪᴏɴ ᴘʟᴀɴ!\n Jᴏɪɴ [PʀᴏBᴏᴛs]({SUPPORT_CHAT}) ᴏʀ DM @ZeoXD ғᴏʀ ᴍᴏʀᴇ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ɢᴇᴛ sᴛᴀʀᴛᴇᴅ !"
-    #     )
+    if message.from_user.id != MAIN_OWNER:
+        return await message.reply_text(
+            f"» 🚀 Wᴀɴᴛ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ʏᴏᴜʀ ᴏᴡɴ ᴍᴇssᴀɢᴇs?\n\nUɴʟᴏᴄᴋ ᴇxᴄʟᴜsɪᴠᴇ ᴀᴄᴄᴇss ᴡɪᴛʜ ᴏᴜʀ sᴜʙsᴄʀɪᴘᴛɪᴏɴ ᴘʟᴀɴ!\n Jᴏɪɴ [PʀᴏBᴏᴛs]({SUPPORT_CHAT}) ᴏʀ DM @ZeoXD ғᴏʀ ᴍᴏʀᴇ ᴅᴇᴛᴀɪʟs ᴀɴᴅ ɢᴇᴛ sᴛᴀʀᴛᴇᴅ !"
+        )
 
     global IS_BROADCASTING
     if message.reply_to_message:
@@ -150,10 +150,6 @@ async def braodcast_message(client, message, _):
             pass
     IS_BROADCASTING = False
 
-    # await app.send_message(
-    #             chat_id=SUB_LOG,
-    #             text=f"#owner \n\n{message.from_user.mention} Jᴜsᴛ Bʀᴏᴀᴅᴄᴀsᴛ ᴀ ᴍᴇssᴀɢᴇ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
-    #         )
 
 
 async def auto_clean():
