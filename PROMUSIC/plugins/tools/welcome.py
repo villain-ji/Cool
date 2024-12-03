72dafdd1f4c515f0741cff3a39ae9fb5d49cb58d
Yasir Akhtar <yasiralam662@gmail.com>
2024-12-03 14:57:21 +0530

    New Photo !

--- PROMUSIC/plugins/tools/welcome.py
+++ PROMUSIC/plugins/tools/welcome.py
@@ -114,14 +114,14 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=50)
     welcome_font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=61)
     
-    draw.text((520, 510), f'{user}', fill=(255, 255, 255), font=font)
-    draw.text((485, 570), f'{id}', fill=(255, 255, 255), font=font)
-    draw.text((565, 650), f"@{uname}", fill=(255, 255, 255), font=font)
+    draw.text((520, 500), f'{user}', fill=(255, 255, 255), font=font)
+    draw.text((485, 560), f'{id}', fill=(255, 255, 255), font=font)
+    draw.text((565, 630), f"@{uname}", fill=(255, 255, 255), font=font)
     # draw.text((2999, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
     # draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
 
     #
-    pfp_position = (130, 120)
+    pfp_position = (102, 115)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
