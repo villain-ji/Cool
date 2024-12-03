6efbd1ee4fc59b72df0dda797e159a8126d99d2d
Yasir Akhtar <yasiralam662@gmail.com>
2024-12-03 14:18:44 +0530

    New Photo !

--- PROMUSIC/plugins/tools/welcome.py
+++ PROMUSIC/plugins/tools/welcome.py
@@ -106,7 +106,7 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("PROMUSIC/assets/welc2.png")
+    background = Image.open("PROMUSIC/assets/welc3.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((635, 635))
@@ -114,9 +114,9 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=70)
     welcome_font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=61)
     
-    draw.text((1230, 500), f'NAME: {user}', fill=(255, 255, 255), font=font)
-    draw.text((1230, 570), f'ID: {id}', fill=(255, 255, 255), font=font)
-    draw.text((1230, 640), f"USERNAME : @{uname}", fill=(255, 255, 255), font=font)
+    draw.text((1035, 750), f'NAME: {user}', fill=(255, 255, 255), font=font)
+    draw.text((1035, 850), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((1035, 950), f"USERNAME : @{uname}", fill=(255, 255, 255), font=font)
     # draw.text((2999, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
     # draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
 
