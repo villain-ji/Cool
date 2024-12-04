695d2afaaa0b552995991620db64bf9f4513eec0
Yasir Akhtar <yasiralam662@gmail.com>
2024-12-04 15:13:42 +0530

    New Poster !

--- PROMUSIC/plugins/tools/welcome.py
+++ PROMUSIC/plugins/tools/welcome.py
@@ -108,7 +108,7 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("PROMUSIC/assets/welc4.png")
+    background = Image.open("PROMUSIC/assets/wel3.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((325, 325))
@@ -116,12 +116,12 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=50)
     welcome_font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=61)
     
-    draw.text((520, 500), f'{user}', fill=(255, 255, 255), font=font)
-    draw.text((485, 560), f'{id}', fill=(255, 255, 255), font=font)
-    draw.text((565, 630), f"@{uname}", fill=(255, 255, 255), font=font)
+    draw.text((624, 978), f'{user}', fill=(255, 255, 255), font=font)
+    draw.text((415, 1150), f'{id}', fill=(255, 255, 255), font=font)
+    draw.text((900, 1275), f"@{uname}", fill=(255, 255, 255), font=font)
 
     #
-    pfp_position = (105, 120)
+    pfp_position = (162, 161)
     background.paste(pfp, pfp_position, pfp)
     background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
