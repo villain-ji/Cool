ef886ecb4adb9d38c322b4947f584c330c01b581
Yasir Akhtar <yasiralam662@gmail.com>
2024-12-04 15:28:40 +0530

    New Poster !

--- PROMUSIC/plugins/tools/welcome.py
+++ PROMUSIC/plugins/tools/welcome.py
@@ -111,7 +111,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     background = Image.open("PROMUSIC/assets/wel3.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
-    pfp = pfp.resize((325, 325))
+    pfp = pfp.resize((650, 650))
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=50)
     welcome_font = ImageFont.truetype('PROMUSIC/assets/font.ttf', size=61)
