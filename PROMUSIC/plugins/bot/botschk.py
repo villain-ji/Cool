08de1122c3f751e8e9ac57eee0dd59e2169616c6
Yasir Akhtar <yasiralam662@gmail.com>
2024-12-06 00:35:08 +0530

    New Bot Check Tool !

--- PROMUSIC/plugins/bot/botschk.py
+++ PROMUSIC/plugins/bot/botschk.py
@@ -1,51 +1,48 @@
-import random
-from pyrogram import filters
+import asyncio
 from PROMUSIC import app
-from PROMUSIC import *
-from ... import *
-import config
-
-from ...logging import LOGGER
-
-from PROMUSIC import app, userbot
-from PROMUSIC.core.userbot import *
-
-import asyncio
-
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import OWNER_ID
-
-import asyncio
+from datetime import datetime
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
-from dotenv import load_dotenv
-from PROMUSIC.core.userbot import Userbot
-from datetime import datetime
+from PROMUSIC.utils.database import get_assistant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Assuming Userbot is defined elsewhere
-userbot = Userbot()
+
+last_checked_time = None
+
+BOT_LIST = ["NezukoProBot", "SiriProBot", "ShizukaProBot"]
 
 
-BOT_LIST = ["NezukoProBot", "SiriProBot", "ShizukaProBot", "NaykaProBot", "NobitaaProBot"]
-
-@app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
+@app.on_message(filters.command("runchk") & filters.user(OWNER_ID))
 async def bots_chk(_, message):
+    global last_checked_time
+
+    # Start the Pyrogram client
+    userbot = await get_assistant(message.chat.id)
+
+    # Get current time before sending messages
+    start_time = datetime.now()
+
     msg = await message.reply_photo(photo="https://telegra.ph/file/4d303296e4fac9a40ea07.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ**\n\n"
     for bot_username in BOT_LIST:
         try:
             bot = await app.get_users(bot_username)
             bot_id = bot.id
-            await asyncio.sleep(0.5)
-            bot_info = await app.send_message(bot_id, "/start")
-            await asyncio.sleep(3)
-            async for bot_message in app.get_chat_history(bot_id, limit=1):
+            await asyncio.sleep(2)
+            await userbot.send_message(bot_id, "/start")
+            await asyncio.sleep(5)
+            async for bot_message in userbot.get_chat_history(bot_id, limit=1):
                 if bot_message.from_user.id == bot_id:
-                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
+                    response += f"╭⎋ {bot.mention}\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
                 else:
-                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
+                    response += f"╭⎋ {bot.mention}\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
         except Exception:
-            response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n"
+            response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n\n"
+
+        # Update last checked time
+        last_checked_time = start_time.strftime("%d-%m-%Y")
     
-    await msg.edit_text(response)
+    await msg.edit_text(f"{response}⏲️ ʟᴀsᴛ ᴄʜᴇᴄᴋ: {last_checked_time}")
                 
