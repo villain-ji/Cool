87ef7115b1b041ceeabddfdfe03849c51f7d2758
Yasir Akhtar <yasiralam662@gmail.com>
2025-01-02 17:43:34 +0530

    New Repo !

--- config.py
+++ config.py
@@ -66,7 +66,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AimmXD/PROMUSIC",
+    "https://github.com/ProPrivates/PROMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
